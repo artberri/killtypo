8603528b64561c3446c7db349b864744fa4f31d8
Alberto Varela <alberto@berriart.com>
2016-11-07 15:28:08 +0100

    Fixing formulas

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,30 +1,54 @@
-export const getWpm = state => {
-  let timings = state.game.timings
-  let charAmount = timings.length
-  let lastTime = timings[charAmount - 1]
-  let startTime = state.game.startTime
-  let delta = lastTime ? (lastTime - startTime) / (60 * 1000) : 0
+function getGameData (state) {
+  return {
+    timings: state.game.timings,
+    charAmount: state.game.timings.length,
+    lastTime: state.game.timings.length > 0 ? state.game.timings[state.game.timings.length - 1] : 0,
+    startTime: state.game.startTime,
+    wrongAmount: state.game.wrong
+  }
+}
+
+function getDelta (startTime, endTime, inMinutes) {
+  let delta = endTime ? (endTime - startTime) / 1000 : 0
+
+  if (inMinutes) {
+    delta = delta / 60
+  }
+
+  return delta
+}
+
+function calculateWpm (delta, charAmount) {
   let wpm = delta > 0 ? (charAmount / 5) / delta : 0
 
   return wpm.toFixed(2)
 }
 
-export const getCpm = state => {
-  let timings = state.game.timings
-  let charAmount = timings.length
-  let lastTime = timings[charAmount - 1]
-  let startTime = state.game.startTime
-  let delta = lastTime ? (lastTime - startTime) / (60 * 1000) : 0
+function calculateCpm (delta, charAmount) {
   let cpm = delta > 0 ? charAmount / delta : 0
 
   return cpm.toFixed(2)
 }
 
+export const getWpm = state => {
+  let data = getGameData(state)
+  let delta = getDelta(data.startTime, data.lastTime, true)
+  let wpm = calculateWpm(delta, data.charAmount)
+
+  return wpm
+}
+
+export const getCpm = state => {
+  let data = getGameData(state)
+  let delta = getDelta(data.startTime, data.lastTime, true)
+  let cpm = calculateCpm(delta, data.charAmount)
+
+  return cpm
+}
+
 export const getAccuracy = state => {
-  let timings = state.game.timings
-  let charAmount = timings.length
-  let wrongAmount = state.game.wrong
-  let accuracy = charAmount / (charAmount + wrongAmount) * 100
+  let data = getGameData(state)
+  let accuracy = data.charAmount / (data.charAmount + data.wrongAmount) * 100
 
   if (!accuracy) {
     accuracy = 100
@@ -34,29 +58,25 @@ export const getAccuracy = state => {
 }
 
 export const getFinalTime = state => {
-  let timings = state.game.timings
-  let charAmount = timings.length
-  let lastTime = timings[charAmount - 1]
-  let startTime = state.game.startTime
+  let data = getGameData(state)
 
-  return lastTime ? lastTime - startTime : 0
+  return data.lastTime ? data.lastTime - data.startTime : 0
 }
 
 export const getWpmChartData = state => {
-  let timings = state.game.timings
-  let charAmount = timings.length
-  let startTime = state.game.startTime
+  let data = getGameData(state)
   let dataPoints = []
 
-  for (let i = 0; i < charAmount; i++) {
-    let time = timings[i]
-    let seconds = (time - startTime) / 1000
-    let delta = time ? seconds / 60 : 0
-    let wpm = delta > 0 ? (charAmount / 5) / delta : 0
+  for (let i = 0; i < data.charAmount; i++) {
+    let time = data.timings[i]
+    let currentCharAmount = i + 1
+    let seconds = getDelta(data.startTime, time, false)
+    let delta = getDelta(data.startTime, time, true)
+    let wpm = calculateWpm(delta, currentCharAmount)
 
     dataPoints.push({
       x: seconds.toFixed(2),
-      y: wpm.toFixed(2)
+      y: wpm
     })
   }
 
@@ -64,20 +84,19 @@ export const getWpmChartData = state => {
 }
 
 export const getCpmChartData = state => {
-  let timings = state.game.timings
-  let charAmount = timings.length
-  let startTime = state.game.startTime
+  let data = getGameData(state)
   let dataPoints = []
 
-  for (let i = 0; i < charAmount; i++) {
-    let time = timings[i]
-    let seconds = (time - startTime) / 1000
-    let delta = time ? seconds / 60 : 0
-    let cpm = delta > 0 ? charAmount / delta : 0
+  for (let i = 0; i < data.charAmount; i++) {
+    let time = data.timings[i]
+    let currentCharAmount = i + 1
+    let seconds = getDelta(data.startTime, time, false)
+    let delta = getDelta(data.startTime, time, true)
+    let cpm = calculateCpm(delta, currentCharAmount)
 
     dataPoints.push({
       x: seconds.toFixed(2),
-      y: cpm.toFixed(2)
+      y: cpm
     })
   }
 
