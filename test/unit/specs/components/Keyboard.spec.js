63006c744a8392f9e02286be1dc82a76f9d1afd0
Alberto Varela <alberto@berriart.com>
2016-11-18 00:48:55 +0100

    Adding tests for blinking cursor

--- test/unit/specs/components/Keyboard.spec.js
+++ test/unit/specs/components/Keyboard.spec.js
@@ -5,13 +5,13 @@ import keyboards from 'src/keyboards/'
 
 Vue.use(Vuex)
 
-function getComponent (Component, optionsState) {
+function getComponent (optionsState) {
   const defaultState = {
     keyboard: 'qwertyus'
   }
   optionsState = optionsState || defaultState
 
-  const Ctor = Vue.extend(Component)
+  const Ctor = Vue.extend(Keyboard)
   const vm = new Ctor({
     store: new Vuex.Store({
       state: {
@@ -32,7 +32,7 @@ describe('Keyboard.vue', () => {
   describe('computed properties', () => {
     describe('keys()', () => {
       it('should return the letters of the chosen keyboard', () => {
-        const vm = getComponent(Keyboard)
+        const vm = getComponent()
 
         expect(vm.keys)
           .to.eql(keyboards['qwertyus'].keys)
@@ -42,7 +42,7 @@ describe('Keyboard.vue', () => {
 
   describe('rendering html content', () => {
     it('should render one key component for each key in keyboard', () => {
-      const vm = getComponent(Keyboard)
+      const vm = getComponent()
 
       expect(vm.$el.querySelectorAll('.keymock').length)
         .to.equal(keyboards['qwertyus'].keys.length)
--- test/unit/specs/components/autocue/BlinkingCursor.spec.js
+++ test/unit/specs/components/autocue/BlinkingCursor.spec.js
@@ -1,3 +1,58 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
 import BlinkingCursor from 'src/components/autocue/BlinkingCursor'
 
-BlinkingCursor
+Vue.use(Vuex)
+
+function getComponent (autocueState) {
+  const defaultState = {
+    isWritting: false
+  }
+  autocueState = autocueState || defaultState
+
+  const Ctor = Vue.extend(BlinkingCursor)
+  const vm = new Ctor({
+    store: new Vuex.Store({
+      state: {
+        autocue: autocueState
+      }
+    })
+  }).$mount()
+
+  return vm
+}
+
+describe('BlinkingCursor.vue', () => {
+  describe('computed properties', () => {
+    describe('isWritting()', () => {
+      it('should return the isWritting state', () => {
+        const vm = getComponent({
+          isWritting: 'hola'
+        })
+
+        expect(vm.isWritting)
+          .to.equal('hola')
+      })
+    })
+  })
+
+  describe('rendering html content', () => {
+    it('should not add the show class when user is not writting', () => {
+      const vm = getComponent({
+        isWritting: false
+      })
+
+      expect(vm.$el.className)
+        .to.equal('')
+    })
+
+    it('should the show class when user is writting', () => {
+      const vm = getComponent({
+        isWritting: true
+      })
+
+      expect(vm.$el.className)
+        .to.equal('show')
+    })
+  })
+})
