6664f22b89ea9f7eedc3a0ba42ff9bab9119b240
Alberto Varela <avarela@avarela-HP.localdomain>
2018-04-12 19:55:14 +0200

    Fix storage.js

--- src/helpers/storage.js
+++ src/helpers/storage.js
@@ -2,15 +2,15 @@ const MARKS = 'killtypo-marks'
 
 export class storage {
   fetch () {
-    var todos = JSON.parse(localStorage.getItem(MARKS) || '[]')
+    var todos = JSON.parse(window.localStorage.getItem(MARKS) || '[]')
     todos.forEach(function (todo, index) {
       todo.id = index
     })
-    todoStorage.uid = todos.length
+
     return todos
   }
 
   save (todos) {
-    localStorage.setItem(MARKS, JSON.stringify(todos))
+    window.localStorage.setItem(MARKS, JSON.stringify(todos))
   }
 }
